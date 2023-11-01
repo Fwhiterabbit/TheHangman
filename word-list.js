0bd442d58024467d1a1abfbcc6b66ae32c8092d6
Fwhiterabbit <thisismichael91@gmail.com>
2023-11-01 16:10:28 +0000

    adding get random word function for display

---
+++ word-list.js
@@ -0,0 +1,82 @@
+const wordList = [
+    {
+        word: "CONTROLLER",
+        hint: "A device used to interact with games or systems."
+    },
+    {
+        word: "HEADSHOT",
+        hint: "A shot to the head, usually resulting in an instant kill in many games."
+    },
+    { 
+        word: "QUEST",
+        hint: "A journey or mission in a game, often involving challenges or tasks to complete."
+    },
+    {
+        word: "LEVEL",
+        hint: "A distinct section of a game, often with increasing difficulty."
+    },
+    {
+        word: "MULTIPLAYER",
+        hint: "A mode allowing multiple players to participate in a game simultaneously."
+    },
+    { 
+        word: "RESPAWN",
+        hint: "The reappearance of a player or character in a game after being defeated."
+    },
+    { 
+        word: "BOSS",
+        hint: "A powerful enemy, typically at the end of a level or stage." 
+    },
+    {
+        word: "PLATFORMER",
+        hint: "A genre of game where the player jumps between suspended platforms." 
+    },
+    { 
+        word: "RPG",
+        hint: "Role-Playing Game, where players assume the roles of characters in a fictional setting."
+    },
+    {
+        word: "MMORPG",
+        hint: "Massively Multiplayer Online Role-Playing Game, allowing many players to participate together." 
+    },
+    { 
+        word: "FRIENDS",
+        hint: "A popular sitcom about a group of friends living in New York City." 
+    },
+    {
+        word: "STRANGERTHINGS",
+        hint: "A science fiction-horror series set in the 1980s." 
+    },
+    { 
+        word: "GAMEOFTHRONES",
+        hint: "A fantasy drama series based on the 'A Song of Ice and Fire' novels by George R.R. Martin." 
+    },
+    { 
+        word: "BREAKINGBAD", 
+        hint: "A crime drama series about a high school chemistry teacher turned methamphetamine manufacturer." 
+    },
+    {
+        word: "SHERLOCK", 
+        hint: "A modern adaptation of the Sherlock Holmes detective stories."
+    },
+    {
+        word: "WESTWORLD",
+        hint: "A science fiction series set in a futuristic Wild-West-themed amusement park." 
+    },
+    {
+        word: "THECROWN",
+        hint: "A historical drama series about the reign of Queen Elizabeth II." 
+    },
+    { 
+        word: "STRANGERTHINGS",
+        hint: "A science fiction-horror series set in the 1980s."
+    },
+    {
+        word: "BLACKMIRROR",
+        hint: "An anthology series exploring the dark side of technological advancement." 
+    },
+    {
+        word: "THEMANDALORIAN",
+        hint: "A space Western series set in the Star Wars universe." 
+    }
+];
